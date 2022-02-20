d64ce805049ba00b9686118e3a4ee43fc9b85419
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-02-20 19:12:02 +0300

    commented out the dotenv import
    - it might be conflicting with how heroku handles vars

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: __dirname + '/.env' });
+// require("dotenv").config({ path: __dirname + '/.env' });
 
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
